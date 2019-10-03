7d6bde5e0e5ffcfe0818db3ba4dd3ab43c8a0354
DariaZubkova <dashazu99@bk.ru>
2019-10-03 23:55:52 +0300

    laba1

--- png_toolkit.cpp
+++ png_toolkit.cpp
@@ -19,6 +19,21 @@ bool png_toolkit::load( const std::string &pictureName )
     return imgData.pixels != nullptr;
 }
 
+void png_toolkit::edit_image()
+{
+	int sizeString = imgData.compPerPixel * imgData.w;
+	for (int i = imgData.h / 2; i < imgData.h; i++) {
+		for (int j = 0; j < sizeString; j+= imgData.compPerPixel) {
+			for (int k = 0; k < imgData.compPerPixel; k++) {
+				if (k == 0)
+					imgData.pixels[k + i * sizeString  + j] = (unsigned char)255;
+				if (k == 1 || k == 2)
+					imgData.pixels[k + i * sizeString + j] = (unsigned char)0;
+			}
+		}
+	}
+}
+
 bool png_toolkit::save( const std::string &pictureName )
 {
     return stbi_write_png(pictureName.c_str(),
