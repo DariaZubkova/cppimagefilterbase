010a8a04644c6eee8b3409be263700baa3bf7646
DariaZubkova <dashazu99@bk.ru>
2019-12-23 14:00:33 +0300

    laba2

---
+++ parseconfig.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+typedef struct configData {
+	string nameFilter;
+	vector<int> coordinates;
+}configData;
+
+class ParseConfig {
+public:
+	vector<configData> readConfig(string configFile);
+};
