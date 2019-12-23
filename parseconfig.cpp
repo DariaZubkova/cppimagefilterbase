79072fd6e855e1e7a1feb9d583c0f7ea3bb60f48
DariaZubkova <dashazu99@bk.ru>
2019-12-23 14:00:54 +0300

    laba2

---
+++ parseconfig.cpp
@@ -0,0 +1,38 @@
+#include "parseconfig.h"
+
+vector<configData> ParseConfig::readConfig(string configFile) {
+	vector<configData> data;
+	char* str = new char[256];
+	ifstream file(configFile);
+	while (!file.eof())
+	{
+		file.getline(str, 256);
+		char* pch = strtok(str, " ");
+
+		int flag = 0, maxFlag = 5;
+		configData cnf;
+		while (pch != NULL)
+		{
+			if (flag == 0) {
+				string s = string(pch);
+				cnf.nameFilter = s;
+			}
+			if (flag != 0) {
+				int ch = (*pch) - '0';
+				cnf.coordinates.push_back(ch);
+			}
+			flag++;
+			if (flag == maxFlag) {
+				flag = 0;
+				data.push_back(cnf);
+				//cnf.nameFilter = NULL;
+				cnf.coordinates.clear();
+			}
+			pch = strtok(NULL, " ");
+		}
+	}
+	file.close();
+	
+
+	return data;
+}
